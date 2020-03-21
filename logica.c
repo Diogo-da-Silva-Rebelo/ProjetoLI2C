d6d53df54ee6a0ac2c8831f85c46f30062c613fa
190Henrique <henriquealvelos@gmail.com>
2020-03-21 19:45:58 +0000

    Reparação na fim jogo;

--- logica.c
+++ logica.c
@@ -55,26 +55,49 @@ int fim_jogo(ESTADO *e){
 
     COORDENADA c = e->ultima_jogada;
     int vencedor;
-    int p1 = verifica_jogada(e,{c.linha+1,c.coluna});
-    int p2 = verifica_jogada(e,{c.linha-1,c.coluna});
-    int p3 = verifica_jogada(e,{c.linha,c.coluna+1});
-    int p4 = verifica_jogada(e,{c.linha,c.coluna-1});
-    int p5 = verifica_jogada(e,{c.linha+1,c.coluna+1});
-    int p6 = verifica_jogada(e,{c.linha-1,c.coluna-1});
+    COORDENADA c1,c2,c3,c4,c5,c6;
+    c1.linha=c.linha+1;
+    c1.coluna=c.coluna;
 
-    if (c.linha==1 && c.coluna=1){
+    c1.linha=c.linha-1;
+    c1.coluna=c.coluna;
+
+    c1.linha=c.linha;
+    c1.coluna=c.coluna+1;
+
+    c1.linha=c.linha;
+    c1.coluna=c.coluna-1;
+
+    c1.linha=c.linha+1;
+    c1.coluna=c.coluna+1;
+
+    c1.linha=c.linha1;
+    c1.coluna=c.coluna-1;
+
+
+    int p1 = verifica_jogada(e,c1);
+    int p2 = verifica_jogada(e,c2);
+    int p3 = verifica_jogada(e,c3);
+    int p4 = verifica_jogada(e,c4);
+    int p5 = verifica_jogada(e,c5);
+    int p6 = verifica_jogada(e,c6);
+
+    if (c.linha==1 && c.coluna==1){
         printf("Parabéns ao Jogador 1!");
         return 1;}
-    else {if (c.linha==8 && c.coluna=8){
-          printf("Parabéns ao Jogador 2!");
-          return 1;}
-          else {if (p1==0 || p2==0 || p3==0 || p4==0 || p5==0 || p6==0) {
-              if (e->jogador_atual==2) {vencedor=1}
-              else vencedor=2;
-              printf("Parabéns ao Jogador %d!",vencedor);
-              return 1;
-          }
-          }
-    }
-    return 0;
+    else {
+        if (c.linha == 8 && c.coluna == 8) {
+            printf("Parabéns ao Jogador 2!");
+            return 1;}
+        else { if (p1 == 0 || p2 == 0 || p3 == 0 || p4 == 0 || p5 == 0 || p6 == 0) {
+                    if (e->jogador_atual == 2)
+                        vencedor = 1;
+                    else {vencedor = 2;}
+
+                    printf("Parabéns ao Jogador %d!", vencedor);
+                    return 1;
+                }
+                else return 0;
+             }
+         }
 }

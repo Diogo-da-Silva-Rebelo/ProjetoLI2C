446bcb5746c69f03be56f68e751249e5d6a1f4b2
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2020-04-24 10:09:56 +0100

    Adição de algumas funções auxíliares e documentação

--- listas.h
+++ listas.h
@@ -4,23 +4,21 @@
 #ifndef PROJETOLI2_LISTAS_H
 #define PROJETOLI2_LISTAS_H
 
+
+/**
+\brief Tipo de dados para as listas.
+*/
 typedef struct nodo {
     void *valor;
     struct nodo *proximo;
 } NODO, *LISTA;
 
 
-// Cria uma lista vazia
 LISTA criar_lista();
-// Insere um valor na cabeça da lista
 LISTA insere_cabeca(LISTA L, void *valor);
-// Devolve a cabeça da lista
 void *devolve_cabeca(LISTA L);
-// Devolve a cauda da lista
 LISTA proximo(LISTA L);
-// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
 LISTA remove_cabeca(LISTA L);
-// Devolve verdareiro se a lista é vazia
 int lista_esta_vazia(LISTA L);
 int tamanho_lista(LISTA L);
 

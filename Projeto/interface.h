67d43b669ce8cb06aeb70df4d0cbd68713ac9ec8
Diogo Rebelo <61949643+Diogo-da-Silva-Rebelo@users.noreply.github.com>
2020-05-01 20:57:22 +0100

    Create interface.h

---
+++ Projeto/interface.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+\brief Estrutura de Interface
+*/
+#ifndef PROJETOLI2_INTERFACE_H
+#define PROJETOLI2_INTERFACE_H
+
+void mostrar_tabuleiro(ESTADO *e,FILE *stdout,int cmd);
+int interpretador(ESTADO *ae,ESTADO *e, FILE *ficheiro);
+void vencedor(int i);
+void prompt(ESTADO *e);
+
+#endif //PROJETOLI2_INTERFACE_H
